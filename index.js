b94f1918f88662577cc30fc56908f052a8ccca3b
Caique Mendonça <132861061+Caique-Mendonca@users.noreply.github.com>
2024-05-06 21:38:02 -0300

    try catch

--- index.js
+++ index.js
@@ -2,26 +2,30 @@
 
 let btnConsultar = document.querySelector(".btn-consultar")
 btnConsultar.addEventListener('click', async function(){
-    console.log("ola mundo");
-    let inputCEP = document.querySelector("#cep-input").value
-    let api = await fetch(`https://viacep.com.br/ws/${inputCEP}/json/`)
-    let apiJson = await api.json()
+    try {
+        let inputCEP = document.querySelector("#cep-input").value
+        let api = await fetch(`https://viacep.com.br/ws/${inputCEP}/json/`)
+        let apiJson = await api.json()
+    
+            let cepHTML = document.querySelector(".cep")
+            let logradouroHTML = document.querySelector(".logradouro")
+            let complementoHTML = document.querySelector(".complemento")
+            let bairroHTML = document.querySelector(".bairro")
+            let localidadeHTML = document.querySelector(".localidade")
+            let ufHTML = document.querySelector(".uf")
+            let dddHTML = document.querySelector(".ddd")
+    
+            console.log(apiJson);
+            cepHTML.innerText = apiJson.cep
+            logradouroHTML.innerText = apiJson.logradouro
+            complementoHTML.innerText = apiJson.complemento
+            bairroHTML.innerText = apiJson.bairro
+            localidadeHTML.innerText = apiJson.localidade
+            ufHTML.innerText = apiJson.uf
+            dddHTML.innerText = apiJson.ddd     
+    } catch (error) {
+        console.error(error)
+    }
 
-        let cepHTML = document.querySelector(".cep")
-        let logradouroHTML = document.querySelector(".logradouro")
-        let complementoHTML = document.querySelector(".complemento")
-        let bairroHTML = document.querySelector(".bairro")
-        let localidadeHTML = document.querySelector(".localidade")
-        let ufHTML = document.querySelector(".uf")
-        let dddHTML = document.querySelector(".ddd")
-
-        console.log(apiJson);
-        cepHTML.innerText = apiJson.cep
-        logradouroHTML.innerText = apiJson.logradouro
-        complementoHTML.innerText = apiJson.complemento
-        bairroHTML.innerText = apiJson.bairro
-        localidadeHTML.innerText = apiJson.localidade
-        ufHTML.innerText = apiJson.uf
-        dddHTML.innerText = apiJson.ddd  
 })
 
